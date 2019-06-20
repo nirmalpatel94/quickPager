ca832e05d47f33de9371cd53aeb25611fee98d4d
npatel <npatel@yext.com>
2019-06-20 11:20:42 -0400

    fix(template v8): listen to pager response instead of refreshing early

--- quickPager/js/popup.js
+++ quickPager/js/popup.js
@@ -1,5 +1,64 @@
 let hasStyle = false;
 
+function savePort(port) {
+  chrome.storage.local.set({ 'port': port }, function() {
+    console.log('Saving Port ' + port);
+  });
+}
+
+function parseHTML(html) {
+  $('#loading').hide();
+  let port = $('#Port-choice').val();
+  $('.Port-error').hide();
+
+  // Initialize the DOM parser
+  let parser = new DOMParser();
+
+  // Parse the text
+  let doc = parser.parseFromString(html, "text/html");
+
+  if (!hasStyle) {
+    $(document.head).append($(doc.head).find('style'));
+    hasStyle = true;
+  }
+
+  $('.Port').siblings().remove();
+
+  // pick the sections we care about
+  $(document.body).append($(doc).find('.Proxy'));
+  $(document.body).append($(doc).find('.Templates'));
+  $(document.body).append($(doc).find('.Tasks'));
+
+  // intercept link clicks and navigate through extension api
+  $('a').click(function(evt) {
+    evt.preventDefault();
+    const href = $(this).attr('href');
+    chrome.tabs.update({
+      url: "https://localhost:" + port + href
+    });
+  });
+
+  // modify form actions to not have relative path
+  $('form').each(function() {
+    if ($(this).attr('action')) {
+      $(this).attr('action', 'https://localhost:' + port + $(this).attr('action'));
+    }
+  });
+
+  // reload the popup html
+  $('form').submit(() => {
+    $('#loading').show();
+  });
+
+  $('form').each(function() {
+    if ($(this).attr('action')) {
+      $(this).ajaxForm(function(resp) {
+        parseHTML(resp);
+      });
+    }
+  });
+}
+
 const fetchContent = () => {
   let port = $('#Port-choice').val();
   fetch('https://localhost:' + port)
@@ -7,49 +66,7 @@ const fetchContent = () => {
     // When the page is loaded convert it to text
     return response.text()
   })
-  .then(function(html) {
-    $('.Port-error').hide();
-
-    // Initialize the DOM parser
-    let parser = new DOMParser();
-
-    // Parse the text
-    let doc = parser.parseFromString(html, "text/html");
-
-    if (!hasStyle) {
-      $(document.head).append($(doc.head).find('style'));
-      hasStyle = true;
-    }
-
-    $('.Port').siblings().remove();
-
-    // pick the sections we care about
-    $(document.body).append($(doc).find('.Proxy'));
-    $(document.body).append($(doc).find('.Templates'));
-    $(document.body).append($(doc).find('.Tasks'));
-
-    // intercept link clicks and navigate through extension api
-    $('a').click(function(evt) {
-      evt.preventDefault();
-      const href = $(this).attr('href');
-      chrome.tabs.update({
-        url: "https://localhost:" + port + href
-      });
-    });
-
-    // modify form actions to not have relative path
-    $('form').each(function() {
-      $(this).attr('action', 'https://localhost:' + port + $(this).attr('action'));
-    });
-
-    // reload the popup html
-    // TODO: make it listen to request and handle it that way
-    $('form').submit(() => {
-      setTimeout(() => {
-        window.location.reload();
-      }, 200);
-    });
-  })
+  .then(parseHTML)
   .catch(function(err) {
     $('.Port-error').show();
     $('.Port').siblings().remove();
@@ -76,8 +93,3 @@ $(document).ready(() => {
   });
 });
 
-function savePort(port) {
-  chrome.storage.local.set({ 'port': port }, function() {
-    console.log('Saving Port ' + port);
-  });
-}
